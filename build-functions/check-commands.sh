b2ac245e519c512ec2f1defe04e43b7a0a9da546
pauloeugenio <pauloeugenio@hotmail.com.br>
2023-11-09 07:35:59 -0500

    removendo

--- build-functions/check-commands.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NEEDED_COMMANDS="curl jq docker skopeo"
-for c in $NEEDED_COMMANDS; do
-  if ! command -v "$c" &>/dev/null; then
-    echo "⚠️  '$c' is not installed. Can't proceed with build."
-    exit 1
-  fi
-done
--- test-configuration/logging.py
+++
@@ -1,4 +0,0 @@
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True
-}
